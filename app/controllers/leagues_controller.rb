7c321ed320f1cb17f9e1cb78224cb550888867ff
Blax Fontaine <valerie.gehle@gmail.com>
2019-11-04 20:33:08 +0100

    generated leagues controller

---
+++ app/controllers/leagues_controller.rb
@@ -0,0 +1,30 @@
+class LeaguesController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @leagues = League.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def set_league
+    @league = League.find(params[:id])
+  end
+end
