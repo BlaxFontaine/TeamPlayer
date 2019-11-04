bf09655c667bad6d5b278ba3ea1cadb20a2f0467
Blax Fontaine <valerie.gehle@gmail.com>
2019-11-04 20:23:42 +0100

    added column Number to players

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_04_190907) do
+ActiveRecord::Schema.define(version: 2019_11_04_192202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_11_04_190907) do
     t.bigint "team_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "number"
     t.index ["team_id"], name: "index_players_on_team_id"
   end
 
