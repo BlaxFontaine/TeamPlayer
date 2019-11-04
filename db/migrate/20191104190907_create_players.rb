d26f0bdfbc6e165b491c0393cf90b31b5f2727ca
Blax Fontaine <valerie.gehle@gmail.com>
2019-11-04 20:09:27 +0100

    generated player model

---
+++ db/migrate/20191104190907_create_players.rb
@@ -0,0 +1,12 @@
+class CreatePlayers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :players do |t|
+      t.string :derby_name
+      t.integer :licence
+      t.string :role
+      t.references :team, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_04_190733) do
+ActiveRecord::Schema.define(version: 2019_11_04_190907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,16 @@ ActiveRecord::Schema.define(version: 2019_11_04_190733) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "players", force: :cascade do |t|
+    t.string "derby_name"
+    t.integer "licence"
+    t.string "role"
+    t.bigint "team_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["team_id"], name: "index_players_on_team_id"
+  end
+
   create_table "teams", force: :cascade do |t|
     t.string "name"
     t.bigint "league_id", null: false
@@ -32,5 +42,6 @@ ActiveRecord::Schema.define(version: 2019_11_04_190733) do
     t.index ["league_id"], name: "index_teams_on_league_id"
   end
 
+  add_foreign_key "players", "teams"
   add_foreign_key "teams", "leagues"
 end
