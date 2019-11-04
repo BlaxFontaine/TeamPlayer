02c383f17805197b77e9f6a9b41a57dd72175d98
Blax Fontaine <valerie.gehle@gmail.com>
2019-11-05 00:34:05 +0100

    possibility to create teams

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -9,9 +9,25 @@ class TeamsController < ApplicationController
   end
 
   def new
+    @league = League.find(params[:league_id])
+    @team = Team.new
   end
 
   def create
+    @team = Team.new(team_params)
+    @league = League.find(params[:league_id])
+    @team.league = @league
+    if @team.save
+      respond_to do |format|
+        format.html { redirect_to league_path(@league) }
+        format.js
+      end
+    else
+      respond_to do |format|
+        format.html { render 'leagues/show' }
+        format.js
+      end
+    end
   end
 
   def edit
@@ -27,5 +43,9 @@ class TeamsController < ApplicationController
 
   def set_team
     @team = Team.find(params[:id])
+  end
+
+  def team_params
+    params.require(:team).permit(:name)
   end
 end
