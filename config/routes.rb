03e5ddc8d13bbcbbf7be898a59cb3051d495e05e
Blax Fontaine <valerie.gehle@gmail.com>
2019-11-15 12:33:27 +0100

    simplified routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,17 +6,18 @@ Rails.application.routes.draw do
     root to: 'devise/sessions#new'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :leagues, except: [:index] do
-    resources :teams
+  # get '/dashboard', to: 'dashboard#league', as: 'dashboard'
+  # resources :dashboards, only: [:show] do
+  #   resources :teams
+  # end
+
+  namespace :dashboard do
+    resources :teams, :players
   end
 
-  resources :teams do
-    resources :players
-  end
-
-  namespace :admin do
-    resources :leagues, only: [:index]
-  end
+  # resources :teams do
+  #   resources :players
+  # end
 
   get '/dashboard', to: 'dashboards#show'
 end
